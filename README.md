c2c9b4b47e5d0e67b1928c57751831c8ef1f6ec0
Abdesamed Bendjeddou <abdou_smd@outlook.com>
2024-07-31 02:04:51 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+"# sharyfi-cli" 
+"# sharyfi-cli" 
---
+++ SharyFi.Cli.Tests/UnitTest1.cs
@@ -0,0 +1,8 @@
+namespace SharyFi.Cli.Tests {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
